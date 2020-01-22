9b514a532a37ea40523811dff322078d2d9cfede
Logon Aluno <logonaluno@local.com>
2020-01-22 20:19:45 -0300

    firs commit

---
+++ src/main/java/br/com/fiap/library/controller/BookController.java
@@ -0,0 +1,12 @@
+package br.com.fiap.library.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookController {
+    @GetMapping
+    public String heelo(){
+        return "Hello World";
+    }
+}

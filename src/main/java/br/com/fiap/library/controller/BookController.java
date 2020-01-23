e19320746cd2624f112452b3ea44993b8da68c44
Logon Aluno <logonaluno@local.com>
2020-01-22 21:27:11 -0300

    get book list by title

--- src/main/java/br/com/fiap/library/controller/BookController.java
+++ src/main/java/br/com/fiap/library/controller/BookController.java
@@ -4,18 +4,20 @@ import br.com.fiap.library.dto.AutorDTO;
 import br.com.fiap.library.dto.BookDTO;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("books")
 public class BookController {
 
     @GetMapping
-    public List<BookDTO> getAll(){
+    public List<BookDTO> getAll(@RequestParam(required = false, value = "title") String titulo){
         List<BookDTO> bookDTOList = new ArrayList<>();
         bookDTOList.add(new BookDTO(
                 1,
@@ -42,6 +44,8 @@ public class BookController {
                 new AutorDTO()
         ));
 
-        return bookDTOList;
+        return bookDTOList.stream()
+                .filter(bookDTO -> titulo== null || bookDTO.getTitulo().startsWith(titulo))
+                .collect(Collectors.toList());
     }
 }

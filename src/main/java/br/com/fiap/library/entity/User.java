e71dedc78b5e493fdc52acba852db4a0556161a9
Logon Aluno <logonaluno@local.com>
2020-02-05 20:36:17 -0300

    Add user

---
+++ src/main/java/br/com/fiap/library/entity/User.java
@@ -0,0 +1,70 @@
+package br.com.fiap.library.entity;
+
+import com.sun.javafx.geom.transform.Identity;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "TB_USER")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    private String username;
+
+    @Column
+    private String password;
+
+    @Column
+    @CreatedDate
+    private Date dataCriacao;
+
+    @Column
+    @LastModifiedDate
+    private Date dataAtualizacao;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(Date dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public Date getDataAtualizacao() {
+        return dataAtualizacao;
+    }
+
+    public void setDataAtualizacao(Date dataAtualizacao) {
+        this.dataAtualizacao = dataAtualizacao;
+    }
+}
